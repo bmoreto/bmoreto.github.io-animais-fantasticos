be40280690eda5e478b96bf88b2b0e396a84db17
bmoreto <beatriz_moreto@hotmail.com>
2024-07-20 15:17:36 -0300

    tooltip

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimalScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initScrolSuave();
 initAnimalScroll();
 initAccordion();
 initTabNav();
 initModal();
+initTooltip();
