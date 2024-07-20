cea68166a931377a43f9f6aa32b4ae61314895a6
bmoreto <beatriz_moreto@hotmail.com>
2024-07-20 14:40:09 -0300

    modal

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initScrolSuave from "./modules/scroll-suave.js";
 import initAnimalScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
 
 initScrolSuave();
 initAnimalScroll();
 initAccordion();
 initTabNav();
+initModal();
