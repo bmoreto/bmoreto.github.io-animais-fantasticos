442b543c5e5d4430d95cfcb7e106b9c6fb95e1de
bmoreto <beatriz_moreto@hotmail.com>
2024-08-17 15:48:39 -0300

    animais fetch

--- js/script.js
+++ js/script.js
@@ -6,8 +6,9 @@ import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropDownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-import initAnimaNumeros from "./modules/anima-numeros.js";
+
 import initFuncionamento from "./modules/funcionamento.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
 
 initScrolSuave();
 initAnimalScroll();
@@ -17,5 +18,5 @@ initModal();
 initTooltip();
 initDropDownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
