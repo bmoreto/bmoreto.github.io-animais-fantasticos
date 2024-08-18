f57b4b853f9080ac8389e6bcda73b5dea9de69dc
bmoreto <beatriz_moreto@hotmail.com>
2024-08-18 06:58:00 -0300

    bitoin fetch

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropDownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
+import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 
 initScrolSuave();
 initAnimalScroll();
@@ -20,3 +20,4 @@ initDropDownMenu();
 initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
+initFetchBitcoin();
